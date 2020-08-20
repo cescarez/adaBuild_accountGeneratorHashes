adbc54aba26825850d7a84eac7c9f02393101f79
Christabel Escarez <christabel.escarez@gmail.com>
2020-08-19 22:25:21 -0700

    got em. ding dong

--- account_gen_hashes.rb
+++ account_gen_hashes.rb
@@ -6,93 +6,22 @@
 #CONSTANTS
 NUM_STUDENTS = 5
 
-student_info = Hash.new()
 #MAIN
 puts "Please enter the full names of #{NUM_STUDENTS} students. "
-i = 0
-student_info[:names] = Array.new 
-NUM_STUDENTS.times do 
+
+student_info = Array.new(NUM_STUDENTS) { {name: "", id: 0, email: ""} } 
+
+student_info.each_with_index do |student, i|
   print "Student #{i + 1}: "
-  student_info[:names] << gets.chomp.strip.upcase
-  until student_info[:names][i].include?(' ')
+  student[:name] << gets.chomp.strip.upcase
+  until student[:name].to_s.include?(' ')
     puts "Entry was not accepted. Please enter both first and last name."
-    student_info[:names][i] = gets.chomp.strip.upcase
+    student[:name] << gets.chomp.strip.upcase
   end
-  i += 1
-end
 
-student_info[:ids] = Array.new(NUM_STUDENTS) { rand(111111..999999) }
+  student[:id] = rand(111111..999999) 
 
-student_info[:emails] = Array.new 
-student_names.each_with_index { |name, i| student_emails << student_names[i][0] + student_names[i].split(' ')[-1] + student_ids[i].to_s[-3..-1] + "@adadevelopersacademy.org" }
-# student_info[:names].each_with_index do |name, i|
-#   split_name = student_info[:names][i].split(' ')
-#   split_id = student_info[:ids][i].to_s.split('')
-#   student_info[:emails] << student_info[:names][i][0] + split_name[-1] + split_id[-3] + split_id[-2] + split_id[-1] + "@adadevelopersacademy.org"
-# end
-
-print "\n"
-printf("%-45s", "Name")
-printf("%-10s", "ID")
-printf("%-45s", "Email")
-print "\n"
-student_names.each_with_index do |name, i|
-  printf("%-45s", name)
-  printf("%-10s", student_ids[i])
-  printf("%-45s", student_emails[i])
-  print "\n"
-
-
-
-
-
-
-## If names to be read into from an external file
-# 1. change the name of the external file in the below code (currently coded as: "names.txt")
-# 2. ensure names are separated by a line-break in the external file
-# 3. Uncomment the below code block and comment out the following code block (end marked with "END BLOCK")
-
-##################### UNCOMMENT THIS CODE TO ADD STUDENT NAMES FROM AN EXTERNAL FILE #############
-external_file_data = File.readlines("names.txt")
-NUM_STUDENTS = external_file_data.length
-
-student_names = []
-external_file_data.each do |name|
-  student_names << name.strip.upcase
-end
-###################################### END BLOCK #########################################
-##################### UNCOMMENT THIS CODE TO MANUALLY ADD STUDENT ACCOUNTS #############
-# #CONSTANTS
-# NUM_STUDENTS = 5
-
-# #MAIN
-# puts "Please enter the full names of #{NUM_STUDENTS} students. "
-
-# student_names = []
-# i = 0
-# NUM_STUDENTS.times do 
-#   print "Student #{i + 1}: "
-#   student_names << gets.chomp.strip.upcase
-#   until student_names[i].include?(' ')
-#     puts "Entry was not accepted. Please enter both first and last name."
-#     student_names[i] = gets.chomp.strip.upcase
-#   end
-#   i += 1
-# end
-###################################### END BLOCK #########################################
-
-student_ids = []
-until student_ids.uniq.length == NUM_STUDENTS
-  id = rand(111111..999999) 
-  while id.to_s[-3..-1].to_i < 100
-    id = rand(111111..999999) 
-  end
-  student_ids << rand(111111..999999) 
-end
-
-student_emails = []
-student_names.each_with_index do |name, i| 
-  split_names = name.split(' ')
+  split_names = student[:name].split(' ')
   split_names.delete(' ')
   if split_names.length > 2
     name_initials = ""
@@ -100,11 +29,21 @@ student_names.each_with_index do |name, i|
       name_initials << partial_name[0] 
     end
     #name_initials includes initial for last name, so range must be used to exclude final character when building email addresses
-    student_emails << name_initials[0..-2] + split_names[-1] + student_ids[i].to_s[-3..-1] + "@adadevelopersacademy.org"
+    student[:email] << name_initials[0..-2] + split_names[-1] + student[:id].to_s[-3..-1] + "@adadevelopersacademy.org"
   else
-    student_emails << student_names[i][0] + split_names[-1] + student_ids[i].to_s[-3..-1] + "@adadevelopersacademy.org"
+    student[:email] << student[:name][i][0] + split_names[-1] + student[:id].to_s[-3..-1] + "@adadevelopersacademy.org"
   end
+
 end
 
-
-end##################################### END BLOCK #########################################
+print "\n"
+printf("%-45s", "Name")
+printf("%-10s", "ID")
+printf("%-45s", "Email")
+print "\n"
+student_info.each_with_index do |student|
+  printf("%-45s", student[:name])
+  printf("%-10s", student[:id])
+  printf("%-45s", student[:email])
+  print "\n"
+end
